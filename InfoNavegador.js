0d70f0658986b63986c25ad6138584e474262a08
uo284373 <98974171+uo284373@users.noreply.github.com>
2022-11-22 19:04:43 +0100

    Delete InfoNavegador.js

--- InfoNavegador.js
+++
@@ -1,6 +0,0 @@
-var navegador = new Object();
-navegador.nombre = navigator.appName;
-navegador.idioma = navigator.language;
-navegador.version = navigator.appVersion;
-navegador.plataforma = navigator.platform;
-navegador.vendedor = navigator.vendor;
